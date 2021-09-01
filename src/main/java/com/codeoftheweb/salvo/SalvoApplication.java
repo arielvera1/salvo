892ab98ba6aa18f6b1fbb1e64e7932ab14ebd00d
PROD\AVERA <aricatupeecu@gmail.com>
2021-09-01 16:00:56 -0300

    tarea 6t

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -158,7 +158,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-
+				.antMatchers("/api/players").permitAll()
 				.antMatchers("/api/login").permitAll()
 				.antMatchers("/api/games").permitAll()
 				.antMatchers("/web/**").permitAll()
